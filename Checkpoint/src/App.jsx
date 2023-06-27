7c533651ab305df2f42eb929783fe6d06e6fb2a0
Gustavo <moraesgb11@hotmail.com>
2023-06-27 19:18:23 -0300

    colocando rotas home e login

--- Checkpoint/src/App.jsx
+++ Checkpoint/src/App.jsx
@@ -1,7 +1,8 @@
 
 import { Outlet } from "react-router-dom";
-import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
 
 function App() {
   return (
@@ -9,11 +10,11 @@ function App() {
       {/* //Na linha seguinte deverá ser feito um teste se a aplicação
         // está em dark mode e deverá utilizar a classe dark ou light */}
       <div className={`app light}`}>
-        <Navbar />
-        <main>
-          <Outlet />
-        </main>
-        <Footer />
+        
+         <Navbar />
+         <Outlet />
+         <Footer/>
+      
       </div>
     </>
   );
--- Checkpoint/src/index.jsx
+++ Checkpoint/src/index.jsx
@@ -2,17 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import Navbar from "./Components/Navbar";
-import Home from "./Routes/Home";
-import Footer from "./Components/Footer";
+import App from "./App";
 import "./index.css";
+import Detail from "./Routes/Detail";
+import Login from "./Routes/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Routes/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //Lembre-se de configurar suas rotas e seu contexto aqui
 root.render(
-  <React.StrictMode>
-    <Navbar />
-    <Home />
-    <Footer />
-  </React.StrictMode>
+   <BrowserRouter>
+    
+    <Routes>
+
+
+      <Route path="/"  element= {<App/>}>
+      <Route path="/Home"  element= {<Home/>}/>
+      <Route path="/Login"  element= {<Login/>}/>
+      <Route path="/Detail"  element= {<Detail/>}/>
+      
+      </Route>
+      
+     
+
+    </Routes>
+    
+   </BrowserRouter>
 );
